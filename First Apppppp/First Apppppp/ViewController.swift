830f0486713e859ba01d3bb5e35290daa1fd9e40
Creighton Fowler Jr <fowlercr1@students.ums-wright.org>
2019-09-18 12:18:08 -0500

    First Commit

---
+++ First Apppppp/First Apppppp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First Apppppp
+//
+//  Created by Fowler, Creighton on 9/18/19.
+//  Copyright © 2019 Fowler, Creighton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
